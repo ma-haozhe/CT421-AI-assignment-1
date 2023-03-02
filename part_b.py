65d7f4c5c8aabe4f750ef17f5c53383b97ac46b2
ma-haozhe <mahaozhe2012@gmail.com>
2023-03-02 20:51:13 +0000

    adding sample data files

--- part_b.py
+++ part_b.py
@@ -2,3 +2,22 @@
 # Haozhe Ma
 # 19280083
 
+# We have a list of Students (S) and a list of Lecturers (L). 
+# Each student has a complete list of the preference of lectures for their project. 
+# and lecutrers have capacities of students they can take. 
+# the fitness is basically the sum of students one lecture finally take.
+# we want the sum to be as small as possible, 
+# this sounds like a knapsack problem to me when first reading it. 
+
+# The student preference and lecturers are stored in two xlsx files. 
+# We first read the supervisor file, store them in lists? we need to get the number of 
+# lecturers first. 
+
+# use pandas lib to read xls tables. 
+import pandas as pd
+#'header=None' to also read the first row.
+dataframe = pd.read_excel('./Supervisors.xlsx', header=None)
+#convert pandas dataframe to list. 
+supervisors = dataframe.values.tolist()
+
+print(supervisors)
